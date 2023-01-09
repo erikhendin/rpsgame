21c112d1bf64e05309793df5c00ee5dda6aa0a67
Erik Hendin <erik.hendin@gmail.com>
2023-01-09 12:09:41 -0500

    added comments for fixes or refinements I could still make - tbd

--- js/rockpaperscissors.js
+++ js/rockpaperscissors.js
@@ -1,16 +1,34 @@
 // Rock, Paper, Scissors
 
+// *********************
+// 1/9/23 12pm EST 
+// Fixes, refinements, improvements, optimizations?
+// At some point I may want a "play again?" link
+// Add validation - what if we want to validate so user cannot enter wrong value/misspelling etc.
+// Add a way for play to choose how many rounds are played
+// Right now I have it saying "round voided" if player enters incorrect value
+// however total rounds it is counting is five (5) even if a round is voided.
+// > Q: should it say number of rounds is 5 if one of those 5 is a 'voided' round
+// OR maybe it counts the round but lists the round as voided.
+// Rounds Played: [Number] | CPU Winner: [Number] | Player Winner | Rounds Voided: [Number]
+// Do we want to change it so that if round is voided, it does NOT count points for either
+// IF Tie means no one gets a point, then what happens if all rounds are a tie?
+// That will mean you could have a game where nobody wins!
+// i.e. 0 to 0 if five consecutive rounds where each round is a tie
+// IF so, then I will also want to update rules in UI of html page 
+// *********************
+
 // Create Array containing the three options 
 const rpsOptions =  ['rock', 'paper', 'scissors'];
 
 // Set up some variables
-let roundResult="";
-let playerSelection="";
-let computerSelection="";
-let roundCount=0;
-let roundWinner="";
-let winsCPU=0;
-let winsPlayer=0;
+let roundResult=""; // result of each round played
+let playerSelection=""; // what player selected
+let computerSelection=""; // what cpu selected
+let roundCount=0; // will store how many rounds have been played.
+let roundWinner=""; // will store who won the round
+let pointsCPU=0; // how many points for CPI
+let pointsPlayer=0; // how many points for Player
 
 // Get Computer's option (random choice)
 const getComputerChoice = (arr) => {
@@ -68,8 +86,8 @@ function playRound(playerSelection, computerSelection) {
           break;
       }
       break;
-    default:
-          console.log("could not understand your selection. Round voided.");
+    default: // if user enters something other than rock paper scissors, void the round
+          console.log("We could not understand your selection. Round voided.");
           roundResult="void";
     }
     console.log("| Result: " + roundResult);
@@ -109,14 +127,14 @@ function playGame() {
     // whoever is winner of the round will get a point recorded
     switch (roundWinner) {
       case "tie":
-        winsCPU++;
-        winsPlayer++;
+        pointsCPU++;
+        pointsPlayer++;
       break;
       case "player":
-        winsPlayer++;
+        pointsPlayer++;
       break;
       case "cpu":
-        winsCPU++;
+        pointsCPU++;
       break;
       case "void":
         roundCount--;
@@ -126,27 +144,28 @@ function playGame() {
   }
   console.log("### GAME RESULTS ###");
   console.log("Num rounds played: " + roundCount);
-  console.log("Player points: " + winsPlayer);
-  console.log("Computer points: " + winsCPU); 
-  if (winsCPU > winsPlayer){
+  console.log("Player points: " + pointsPlayer);
+  console.log("Computer points: " + pointsCPU); 
+  if (pointsCPU > pointsPlayer){
     console.log("CPU Won! Better luck next time");      
-  } else if (winsPlayer > winsCPU) {
+  } else if (pointsPlayer > pointsCPU) {
     console.log("Player Wins! Congratulations!");
-  } else if (winsPlayer === winsCPU) {
+  } else if (pointsPlayer === pointsCPU) {
     console.log("Tie Game! Amazingly close.")
   } else {
     console.log("Something went wrong - please try again");
   }
 }
 
+// reset counts at the beginning of the game
 function resetStats(){
   roundResult="Round Outcome: ";
   playerSelection="";
   computerSelection="";
   roundCount=0;
   roundWinner="";
-  winsCPU=0;
-  winsPlayer=0;
+  pointsCPU=0;
+  pointsPlayer=0;
 }
 
 // playGame();
